e167a453620792a7857f9312f3a4f73f0d772a08
cesarapm <garyfeomm@gmail.com>
2023-06-10 14:07:56 -0500

    Add your commit

--- routes/index.js
+++ routes/index.js
@@ -17,7 +17,9 @@ router.post("/send-email", async (req, res) => {
         </br>
         </br>
         </br>
-       
+        <p>https://api.whatsapp.com/send?phone=${phone}&text=Buen%20d%C3%ADa%2C%20me%20interesa%20saber%20m%C3%A1s%20informaci%C3%B3n%20para%20tu%20sitio%20Web</p>
+
+
     `;
 
   let transporter = nodemailer.createTransport({
@@ -39,8 +41,6 @@ router.post("/send-email", async (req, res) => {
     subject: "Website Contact Form",
 
     html: contentHTML,
-
-    body: `https://api.whatsapp.com/send?phone=${phone}&text=Buen%20d%C3%ADa%2C%20me%20interesa%20saber%20m%C3%A1s%20informaci%C3%B3n%20para%20tu%20sitio%20Web`,
   });
 
   res.json(info);
