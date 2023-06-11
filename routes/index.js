9339860de9c3301d815da6ac4a5c1eb962bc2201
cesarapm <garyfeomm@gmail.com>
2023-06-10 22:12:49 -0500

    Add your commit

--- routes/index.js
+++ routes/index.js
@@ -21,6 +21,21 @@ router.post("/send-email", async (req, res) => {
         <a href="https://api.whatsapp.com/send?phone=${phone}&text=Buen%20d%C3%ADa%2C%20me%20interesa%20saber%20m%C3%A1s%20informaci%C3%B3n%20para%20tu%20sitio%20Web">Link Whatsapp</a>
 
     `;
+  contentHTML2 = `
+    <h1>Buen día.</h1>
+  
+        <h3>Hola ${name}</h3>
+        <h3>Recibimos tu Mensaje sobre Inf de Servicio de programación web.</h3>
+        <h3>Si deseas mas Inf nos puedes Contactar vía WhatsApp: <a href="https://acortar.link/7KYawS">Link Whatsapp</a>
+        </h3>
+        <h3>o por este medio</h3>
+        <h3>Saludos</h3>
+
+  
+
+   
+   
+`;
 
   let transporter = nodemailer.createTransport({
     host: "smtp.hostinger.com",
@@ -31,19 +46,30 @@ router.post("/send-email", async (req, res) => {
       pass: "Webapm123%",
     },
     tls: {
-      rejectUnauthorized: false,
+      rejectUnauthorized: true,
     },
   });
 
   let info = await transporter.sendMail({
-    from: "Nuevo Contacto", // sender address,
-    to: "geoapm@hotmail.com",
+    from: "contacto@webgeoapm.com", // sender address,
+    //to: "geoapm@hotmail.com",
+    to: "garyfeomm@gmail.com",
     subject: "Website Contact Form",
 
     html: contentHTML,
   });
 
-  res.json(info);
+  let info2 = await transporter.sendMail({
+    from: "contacto@webgeoapm.com", // sender address,
+    // to: "geoapm@hotmail.com",
+    to: email,
+    subject: "Somos Desarrolladores Web",
+
+    html: contentHTML2,
+  });
+
+  //res.json(info);
+  res.json(info2);
 });
 
 module.exports = router;
